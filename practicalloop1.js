9ea4257ffff249a28fec3f3e655a98685cefe652
Alexandria <walkerciara24@gmail.com>
2024-09-24 14:34:32 -0400

    Practical Loop Info

---
+++ practicalloop1.js
@@ -0,0 +1,31 @@
+//Understanding of loops and iterations in JS through challenging exercises//
+//Part 1 : Loop Concepts Overview
+
+//Example/Skelton For Loop, While Loop, Do...While Loop//
+
+//Example for let//
+for (let i = 2; i <5 i++) {
+    console.log (i);
+}
+//Example for while//
+let i = 3;
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+//Do...While Example//
+let i = 0;
+do {
+console.log (i);
+i++;
+} while (i < 5);
+
+//Part 2 Practical Exercise//
+//Multiplication Table for Loop//
+
+for (let i = 1; i >=10; i++) {
+console.log ('Table of Multiplication for the {i}:')
+}
+    for(let a = 1; a <){
+console.log
+    }
